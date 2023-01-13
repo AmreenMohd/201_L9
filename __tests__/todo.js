ab61c62a4796c53a895f82537cbd4f044f2c1f89
AmreenMohd <113177951+AmreenMohd@users.noreply.github.com>
2023-01-13 23:17:57 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -36,9 +36,9 @@ describe("Todo test cases ", () => {
     let res = await agent.get("/signup");
     const csrfToken = extractCsrfToken(res);
     res = await agent.post("/users").send({
-      firstName: "Test",
-      lastName: "User A",
-      email: "user.a@test.com",
+      firstName: "Amreen",
+      lastName: "A",
+      email: "amreena@test.com",
       password: "12345678",
       _csrf: csrfToken,
     });
